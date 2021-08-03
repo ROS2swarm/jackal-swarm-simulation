091e1be99cc203639eda75d8cd54c2f938816467
Tavia Plattenteich <you@bla.com>
2021-08-03 14:05:53 +0200

    fixed copy paste errors

--- multi_jackal_base/launch/Jackal_launch_multi.launch.py
+++ multi_jackal_base/launch/Jackal_launch_multi.launch.py
@@ -4,7 +4,7 @@ import time
 
 # start gazebo
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-cli_args = ['/home/user/catkin_ws/src/launch_jackal_gazebo/launch/jackal.launch']
+cli_args = ['/home/user/catkin_ws/src/jackal-swarm-addition/multi_jackal_base/launch/jackal_gazebo.launch']
 roslaunch_args = cli_args[1:]
 roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
 gazebo = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
@@ -14,7 +14,7 @@ time.sleep(3)
 # spawn jackals
 roslaunch_files = []
 for num in range(0, int(sys.argv[1])):
-    cli_args = ['/home/user/catkin_ws/src/multi_jackal_simulation_rob/multi_jackal_base/launch/base_jackal.launch',
+    cli_args = ['/home/user/catkin_ws/src/jackal-swarm-addition/multi_jackal_base/launch/base_jackal.launch',
                 'ns:=robot_namespace_' + str(num), 'y:='+str(num)]
     roslaunch_args = cli_args[1:]
     node = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args
